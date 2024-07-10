cd0ade3a5ed95dec5d9db80358a2a8635ffe2095
hun2zz <tkdgnsdldkdlel@naver.com>
2024-07-10 23:44:42 +0900

    배포환경 준비

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Home from "./pages/Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Products from "./pages/Products";
+import RootLayout from "./layout/RootLayout";
+import ErrorPage from "./pages/ErrorPage";
+import Events, { loader as eventListLoader } from "./pages/Events";
+import EventDetail, {
+  loader as eventDetailLoader,
+  action as deleteAction,
+} from "./pages/EventDetail";
+import EventLayout from "./layout/EventLayout";
+import NewEvent from "./pages/NewEvent";
+import EditPage from "./pages/EditPage";
+import { action as saveAction } from "./components/EventForm";
 
-function App() {
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout></RootLayout>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      { index: true, element: <Home /> },
+      {
+        path: "events",
+        element: <EventLayout></EventLayout>,
+        children: [
+          {
+            index: true,
+            element: <Events />,
+
+            //이페이지가 열릴 때 자동으로 트리거되어 호출되는 함수
+            //이 함수에는 페이지가 열리자마자 해야할 일을 적을 수 있음.
+            // loader: eventListLoader,
+          },
+          {
+            path: ":prodId",
+
+            loader: eventDetailLoader,
+            id: "event-detail",
+            children: [
+              { index: true, element: <EventDetail />, action: deleteAction },
+              { path: "edit", element: <EditPage />, action: saveAction },
+            ],
+          },
+          { path: "new", element: <NewEvent />, action: saveAction },
+        ],
+      },
+    ],
+  },
+]);
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <RouterProvider router={router} />
     </div>
   );
-}
+};
 
 export default App;
