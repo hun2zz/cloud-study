5dd58ebefc793332d64cf02672643333a5689794
hun2zz <tkdgnsdldkdlel@naver.com>
2024-07-12 10:33:15 +0900

    ✅ 사용자 검증

--- src/components/auth/SignUpForm.js
+++ src/components/auth/SignUpForm.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import styles from "./SignUpForm.module.scss";
 import EmailInput from "./EmailInput";
-import ProgressBar from "../ProgressBar";
 import VerificationInput from "./VarificationInput";
+import ProgressBar from "../ProgressBar";
 
 const SignUpForm = () => {
   // 현재 몇 단계가 진행되고 있는지
@@ -11,9 +11,13 @@ const SignUpForm = () => {
   // 단계가 성공적으로 완료되었는지
   const [success, setSuccess] = useState(false);
 
+  // 입력된 이메일
+  const [enteredEmail, setEnteredEmail] = useState("");
+
   // 이메일 중복확인이 끝났을 때 호출될 함수
-  const emailSuccessHandler = () => {
+  const emailSuccessHandler = (email) => {
     setSuccess(true);
+    setEnteredEmail(email);
 
     setTimeout(() => {
       setStep(2);
@@ -26,7 +30,7 @@ const SignUpForm = () => {
       <div className={styles.formStepActive}>
         {step === 1 && <EmailInput onSuccess={emailSuccessHandler} />}
 
-        {step === 2 && <VerificationInput />}
+        {step === 2 && <VerificationInput email={enteredEmail} />}
 
         {success && <ProgressBar />}
       </div>
